f39c107ca5171231cf9e3e522a6132e3f57e8f9b
unknown <magoyeison@gmail.com>
2019-09-29 21:10:53 -0400

    import heroku config

--- dj_chat/settings.py
+++ dj_chat/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import json
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
