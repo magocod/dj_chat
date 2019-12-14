c0c6d71cc63b5918a7f3ee596d75d0f69ab968d0
unknown <magoyeison@gmail.com>
2019-12-14 11:09:22 -0400

    django v3 update

--- dj_chat/asgi.py
+++ dj_chat/asgi.py
@@ -1,13 +1,16 @@
 """
-ASGI entrypoint. Configures Django and then runs the application
-defined in the ASGI_APPLICATION setting.
+ASGI config for dj3 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-import django
-from channels.routing import get_default_application
+
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_chat.settings')
-django.setup()
 
-application=get_default_application()
+application = get_asgi_application()
