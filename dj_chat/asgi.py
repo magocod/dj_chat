d93c8c5181856b0c471f8d7425801f85872d62e6
unknown <magoyeison@gmail.com>
2020-01-14 17:38:41 -0400

    asgi daphne config

--- dj_chat/asgi.py
+++ dj_chat/asgi.py
@@ -6,26 +6,24 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
+# import os
 
-import os
+# from django.core.asgi import get_asgi_application
 
-from django.core.asgi import get_asgi_application
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_chat.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_chat.settings')
-
-application = get_asgi_application()
+# application = get_asgi_application()
 
 """
 ASGI entrypoint. Configures Django and then runs the application
 defined in the ASGI_APPLICATION setting.
 """
+import os
 
-# import os
+import django
+from channels.routing import get_default_application
 
-# import django
-# from channels.routing import get_default_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_chat.settings')
+django.setup()
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_chat.settings')
-# django.setup()
-
-# application=get_default_application()
+application=get_default_application()
