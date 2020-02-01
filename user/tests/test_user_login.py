c9f99bf72f61176b4e04f74e74d039519c8fd4c8
unknown <magoyeison@gmail.com>
2020-02-01 18:24:42 -0400

    remove redefinition 'test_logout'

--- user/tests/test_user_login.py
+++ user/tests/test_user_login.py
@@ -60,15 +60,6 @@ def test_logout(admin_client):
 
 
 @pytest.mark.users_authentication
-def test_logout(admin_client):
-    """
-    ...
-    """
-    response = admin_client.post('/api/user/logout/')
-    assert response.status_code == 200
-
-
-@pytest.mark.users_authentication
 def test_repeat_logout(public_client):
     """
     ...
