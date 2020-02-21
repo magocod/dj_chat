4dd166b840abfa6bb6b145e7cb98133245955780
unknown <magoyeison@gmail.com>
2020-02-21 19:32:13 -0400

    update version 1 -> 2

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dj_chat", version="1.0.0", packages=find_packages(), python_requires=">=3.6",
+    name="dj_chat", version="2.0.0", packages=find_packages(), python_requires=">=3.6",
 )
