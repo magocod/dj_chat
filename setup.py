5145a360e9ccd09afd722f1f3d057e36f92ac46f
unknown <magoyeison@gmail.com>
2020-03-19 13:21:23 -0400

    setup details configuration

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 from setuptools import find_packages, setup
 
+from dj_chat import __version__
+
 setup(
-    name="dj_chat", version="2.0.0", packages=find_packages(), python_requires=">=3.6",
+    name="dj_chat",
+    version=__version__,
+    url="https://github.com/magocod/dj_chat",
+    author="Yson",
+    author_email="androvirtual12@gmail.com",
+    description="basic chat application, using websockets",
+    python_requires=">=3.7",
+    packages=find_packages(),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
