740e955d76d4e16b36a9914ed379ca94c611f7fe
yson <magoyeison@gmail.com>
2020-06-18 19:22:00 -0500

    update all dependencies

--- rsa_auth/read_keys.py
+++ rsa_auth/read_keys.py
@@ -2,42 +2,42 @@ from datetime import datetime, timedelta, timezone
 
 import jwt
 
-private_key = ''
-public_key = ''
+private_key = ""
+public_key = ""
 
-with open('rsa-private.key', 'r') as privatefile:
-  # print(privatefile)
-  lines = privatefile.readlines()
-  length = len(lines)
-  for index, line in enumerate(lines):
-    # print(line)
-    if index == 0:
-      private_key = private_key + f"\n{line}" 
-      continue
+with open("rsa-private.key", "r") as privatefile:
+    # print(privatefile)
+    lines = privatefile.readlines()
+    length = len(lines)
+    for index, line in enumerate(lines):
+        # print(line)
+        if index == 0:
+            private_key = private_key + f"\n{line}"
+            continue
 
-    if index == length:
-      private_key = private_key + f"\n{line}"
-      continue
+        if index == length:
+            private_key = private_key + f"\n{line}"
+            continue
 
-    private_key = private_key + line
+        private_key = private_key + line
 
 print(private_key)
 
-with open('rsa-public.key', 'r') as publicfile:
-  # print(publicfile)
-  lines = publicfile.readlines()
-  length = len(lines)
-  for index, line in enumerate(lines):
-    # print(line)
-    if index == 0:
-      public_key = public_key + f"\n{line}" 
-      continue
+with open("rsa-public.key", "r") as publicfile:
+    # print(publicfile)
+    lines = publicfile.readlines()
+    length = len(lines)
+    for index, line in enumerate(lines):
+        # print(line)
+        if index == 0:
+            public_key = public_key + f"\n{line}"
+            continue
 
-    if index == length:
-      public_key = public_key + f"\n{line}"
-      continue
+        if index == length:
+            public_key = public_key + f"\n{line}"
+            continue
 
-    public_key = public_key + line
+        public_key = public_key + line
 
 print(public_key)
 
@@ -48,10 +48,12 @@ timestamp = expired_time.replace(tzinfo=timezone.utc).timestamp()
 
 print(clain_day)
 print(expired_time)
-print('timestamp', timestamp)
+print("timestamp", timestamp)
 print(datetime.fromtimestamp(timestamp))
 
-encoded = jwt.encode({'exp': timestamp, 'some': 'payload'}, private_key, algorithm='RS256')
-print('jwt', encoded)
-decoded = jwt.decode(encoded, public_key, algorithms='RS256')
-print('decoded', decoded)
+encoded = jwt.encode(
+    {"exp": timestamp, "some": "payload"}, private_key, algorithm="RS256"
+)
+print("jwt", encoded)
+decoded = jwt.decode(encoded, public_key, algorithms="RS256")
+print("decoded", decoded)
