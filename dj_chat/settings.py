d94e972a4a3945358484af7ae2d1b064d4203935
yson <magoyeison@gmail.com>
2020-09-12 16:11:52 -0400

    open-api docs

--- dj_chat/settings.py
+++ dj_chat/settings.py
@@ -186,18 +186,18 @@ https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 """
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    # },
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "djchat",
-        "USER": "root",
-        "PASSWORD": "",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
-    }
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    },
+    # "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "NAME": "djchat",
+    #     "USER": "root",
+    #     "PASSWORD": "",
+    #     "HOST": "127.0.0.1",
+    #     "PORT": "3306",
+    # }
 }
 
 CHANNEL_LAYERS = {
